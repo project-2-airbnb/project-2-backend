d918d8bd69e01ca39b759307b4c69f07556c5da0
Rian Darmawan <riandarmawan785@gmail.com>
2024-05-21 07:01:39 +0700

    struktur awal feature user

--- features/users/handler/handler-response.go
+++ features/users/handler/handler-response.go
@@ -1,1 +1,9 @@
 package handler
+
+type UserResponse struct {
+	FullName       string `json:"fullname"`
+	Email          string `json:"email"`
+	Address        string `json:"address"`
+	PhoneNumber    string `json:"phone_number"`
+	PictureProfile string `json:"picture_profile"`
+}

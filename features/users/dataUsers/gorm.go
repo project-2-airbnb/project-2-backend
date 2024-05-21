6a2c69d129dc9c4913c02ce69c7e8344a38cc353
Rian Darmawan <riandarmawan785@gmail.com>
2024-05-21 09:37:14 +0700

    feature register

--- features/users/dataUsers/gorm.go
+++ features/users/dataUsers/gorm.go
@@ -4,20 +4,21 @@ import (
 	datareservations "project-2/features/reservations/dataReservations"
 	datareview "project-2/features/review/dataReview"
 	datarooms "project-2/features/rooms/dataRooms"
+	"project-2/features/users"
 
 	"gorm.io/gorm"
 )
 
 type Users struct {
 	gorm.Model
-	FullName       string                          `json:"fullname"`
-	Email          string                          `json:"email"`
-	Password       string                          `json:"password"`
-	RetypePassword string                          `json:"retype_password"`
-	Address        string                          `json:"address"`
-	PhoneNumber    string                          `json:"phone_number"`
-	PictureProfile string                          `json:"picture_profile"`
-	UserType       string                          `json:"user_type"`
+	FullName       string
+	Email          string `gorm:"unique"`
+	Password       string
+	RetypePassword string
+	Address        string
+	PhoneNumber    string
+	PictureProfile string
+	UserType       string
 	Rooms          []datarooms.Rooms               `gorm:"foreignKey:UserID"`
 	Reservations   []datareservations.Reservations `gorm:"foreignKey:UserID"`
 	Reviews        []datareview.Reviews            `gorm:"foreignKey:UserID"`
@@ -25,4 +26,18 @@ type Users struct {
 
 func (u Users) IsValidRole() bool {
 	return u.UserType == "customer" || u.UserType == "hosting"
+}
+
+func (u Users) ModelToUser() users.User {
+	return users.User{
+		UserID:         u.ID,
+		FullName:       u.FullName,
+		Email:          u.Email,
+		Address:        u.Address,
+		PhoneNumber:    u.PhoneNumber,
+		PictureProfile: u.PictureProfile,
+		UserType:       u.UserType,
+		CreatedAt:      u.CreatedAt,
+		UpdatedAt:      u.UpdatedAt,
+	}
 }
